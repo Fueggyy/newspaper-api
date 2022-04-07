6c917c76a8d69941f4318f3029ef61b956d15985
Rafif Huda <luckqmaks@gmail.com>
2022-04-07 20:52:46 +0700

    firts commit

---
+++ index.js
@@ -0,0 +1,80 @@
+const PORT = process.env.PORT || 8000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+const app = express()
+
+const newspapers = [
+    {
+        name: 'detik',
+        address: 'https://www.detik.com/tag/invasi-rusia-ke-ukraina',
+        base: ''
+    },
+    {
+        name: 'kompas',
+        address: 'https://www.kompas.com/tag/rusia',
+        base: ''
+    },
+
+]
+
+const articles =[]
+
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("Rusia")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+
+                articles.push({ 
+                    title,
+                    url: newspaper.base + url,
+                    source: newspaper.name
+                })
+            })
+
+        })
+})
+
+app.get('/', (req, res) => {
+    res.json("hello")
+})
+
+app.get('/news', (req, res) => {
+    res.json(articles)
+})
+
+app.get('/news/:newspaperId', (req, res) => {
+    const newspaperId = req.params.newspaperId
+
+    const newspaperAddress = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].address
+    const newspaperBase = newspapers.filter(newspaper => newspaper.name == newspaperId)[0].base
+
+
+    axios.get(newspaperAddress)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const specificArticles = []
+
+            $('a:contains("Rusia")', html).each(function () {
+                const title = $(this).text()
+                const url = $(this).attr('href')
+                specificArticles.push({
+                    title,
+                    url: newspaperBase + url,
+                    source: newspaperId
+                })
+            })
+            res.json(specificArticles)
+        }).catch(err => console.log(err))
+})
+
+
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
